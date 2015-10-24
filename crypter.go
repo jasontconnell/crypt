ba394595a110ee20d877f742ea6ce698ab0fe879
Jason Connell <jconnell@delphicdigital.com>
2015-10-23 23:19:26 -0400

    Fixing crypt, add serverinfo

--- crypter.go
+++ crypter.go
@@ -7,7 +7,7 @@ import (
 	"crypto/md5"
 	"encoding/base64"
 	"io"
-	//"fmt"
+	"fmt"
 )
 
 func md5sum(d []byte) []byte {
@@ -17,27 +17,30 @@ func md5sum(d []byte) []byte {
 }
 
 func evpBytesToKey(password string, keyLen int) (key []byte) {
-	const md5Len = 16
+    const md5Len = 16
 
-	cnt := (keyLen-1)/md5Len + 1
-	m := make([]byte, cnt*md5Len)
-	copy(m, md5sum([]byte(password)))
+    cnt := (keyLen-1)/md5Len + 1
+    m := make([]byte, cnt*md5Len)
+    key = make([]byte, keyLen)
 
-	// Repeatedly call md5 until bytes generated is enough.
-	// Each call to md5 uses data: prev md5 sum + password.
-	d := make([]byte, md5Len+len(password))
-	start := 0
-	for i := 1; i < cnt; i++ {
-		start += md5Len
-		copy(d, m[start-md5Len:start])
-		copy(d[md5Len:], password)
-		copy(m[start:], md5sum(d))
-	}
-	return m[:keyLen]
+    copy(m, md5sum([]byte(password)))
+
+    // Repeatedly call md5 until bytes generated is enough.
+    // Each call to md5 uses data: prev md5 sum + password.
+    d := make([]byte, md5Len+len(password))
+    start := 0
+    for i := 1; i < cnt; i++ {
+        start += md5Len
+        copy(d, m[start-md5Len:start])
+        copy(d[md5Len:], password)
+        copy(m[start:], md5sum(d))
+    }
+    return m[:keyLen]
 }
 
 func Encrypt(key string, text []byte) string {
-    block, err := aes.NewCipher(evpBytesToKey(key,32))
+    bkey := evpBytesToKey(key,32)
+    block, err := aes.NewCipher(bkey)
     if err != nil {
         panic(err)
     }
@@ -49,20 +52,23 @@ func Encrypt(key string, text []byte) string {
     cfb := cipher.NewCFBEncrypter(block, iv)
     cfb.XORKeyStream(ciphertext[aes.BlockSize:], text)
     return encodeBase64(ciphertext)
+
 }
 
-
 func Decrypt(key string, b64 string) string {
+    bkey := evpBytesToKey(key,32)
     text := decodeBase64(b64)
-    block, err := aes.NewCipher(evpBytesToKey(key,32))
+    block, err := aes.NewCipher(bkey)
     if err != nil {
         panic(err)
     }
     if len(text) < aes.BlockSize {
         panic("ciphertext too short")
     }
+
     iv := text[:aes.BlockSize]
     text = text[aes.BlockSize:]
+
     cfb := cipher.NewCFBDecrypter(block, iv)
     cfb.XORKeyStream(text, text)
     return string(text)
--- crypter_test.go
+++ crypter_test.go
@@ -8,8 +8,9 @@ import (
 func TestEncrypt(t *testing.T){
 	key := "random array of characters"
 
-	s := Encrypt(key, []byte("jason123"))
+	s := Encrypt(key, []byte("abopassword"))
 
+	t.Log("Encrypted abopassword")
 	t.Log(s)
 	fmt.Println(s)
 }
@@ -17,9 +18,10 @@ func TestEncrypt(t *testing.T){
 func TestDecrypt(t *testing.T){
 	key := "random array of characters"
 
-	s := Decrypt(key, "dkYsRBDNTMujVjmSy59NiJjhg9bSpPtN")
+	//s := Decrypt(key, "hg+TLS1yFfXaGaW3t7Cc0Q==")
 
-	fmt.Println(s)
-	t.Log(s)
+	t.Log("aFsycP-HU62YL8yLXRC5lw==", Decrypt(key, "aFsycP+HU62YL8yLXRC5lw=="))
+	t.Log("Lj9w6mPQNKEE5KSA9KeAXtRCA99WAGOkBRfm", Decrypt(key, "Lj9w6mPQNKEE5KSA9KeAXtRCA99WAGOkBRfm"))
+	t.Log("-qNuRpQKjGbGleL_8Efuhg==", Decrypt(key, "+qNuRpQKjGbGleL/8Efuhg=="))
 }
 
